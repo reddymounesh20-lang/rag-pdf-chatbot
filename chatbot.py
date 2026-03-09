a1a18bed29b3fdf3593d9989d7a4b1b3c925af7e
reddymounesh20-lang <reddymounesh20@gmail.com>
2026-03-09 12:29:15 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,6 @@
 from openai import OpenAI
 import os
 
-client = OpenAI(api_key="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")
 
 print("AI Chatbot (type 'exit' to quit)")
 
@@ -23,5 +22,6 @@ while True:
 
     reply = response.choices[0].message.content
     print("Bot:", reply)
+
 
     messages.append({"role": "assistant", "content": reply})
